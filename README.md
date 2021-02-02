74dd3b56e87cdd8725b83a3122d27c30d56be5c6
VitorGGA <vitorgga@gmail.com>
2021-02-01 22:55:13 -0400

    initial

---
+++ README.md
@@ -0,0 +1,36 @@
+# MySQL Binlog Analyzer
+
+This application show usage from binlog extract files.
+
+This use regular expressions to show the usage.
+
+(c) This service is created using Symfony Command
+
+## How to run
+
+Using command line:
+
+```
+php index.php analyzer
+```
+
+Optional set filename on call command:
+
+```
+php index.php analyzer file.csv
+```
+
+## Preview
+
+![Alt text](preview.png "Preview")
+
+
+## Contribute
+
+Contributions are always welcome!
+
+## License
+
+[![CC0](https://licensebuttons.net/p/zero/1.0/88x31.png)](https://creativecommons.org/publicdomain/zero/1.0/)
+
+To the extent possible under law, [VitorGGA](https://vitorgga.com) has waived all copyright and related or neighboring rights to this work.
---
+++ app/Command/MysqlBinlogAnalyzer.php
@@ -0,0 +1,108 @@
+<?php
+namespace App\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Helper\ProgressBar;
+
+class MysqlBinlogAnalyzer extends Command
+{
+    protected static $defaultName = 'analyzer';
+
+    protected function configure()
+    {
+        $this
+            ->setName('analyzer')
+            ->setDescription('MySQL Binlog Analyzer')
+            ->setHelp('This command show binlog usage')
+            ->addArgument('file', InputArgument::OPTIONAL, 'The name of the file to analyze');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        // CLEAR CONSOLE
+        $output->write(sprintf("\033\143"));
+
+        // IO START
+        $io = new SymfonyStyle($input, $output);
+        $io->title('MySQL Binlog Analyzer');
+
+        // FILE
+        $file = $input->getArgument('file') ?? 'example.csv';
+        $file = $io->ask('File to analyzer?', $file) ?? $file;
+
+        $io->note("File: {$file}");
+
+        // EXTRACT
+        $a_total = self::extract($io, $file);
+
+        // PRINTER
+        self::printer($io, $output, $a_total);
+
+        return Command::SUCCESS;
+    }
+
+    private function printer($io, $output, $a_total = [])
+    {
+        $total = array_sum($a_total);
+
+        $io->note("Total: {$total} transactions");
+
+        foreach ($a_total as $key => $value) {
+
+            $max = $max ?? $value;
+
+            $progressBar = new ProgressBar($output, $total);
+            $progressBar->setEmptyBarCharacter(' ');
+            $progressBar->setProgressCharacter('|');            
+            $progressBar->advance($value);
+
+            $output->write("  {$key}");
+            $io->newLine();
+        }
+
+        $io->newLine();
+    }
+
+    private function extract($io, $file = '')
+    {
+        if (!file_exists($file)) {
+            $io->error("File not found");
+            return [];
+        }
+
+        $content = file_get_contents($file);
+
+        $a_lines = explode("\n", $content);
+
+        $a_total = [];
+
+        foreach ($a_lines as $line) {
+
+            $a_expressions = [
+                "(INSERT INTO)(.*?)(\()",
+                "(REPLACE INTO)(.*?)(\()",
+                "(UPDATE)(.*?)(SET)",
+                "(DELETE FROM)(.*?)(WHERE)",
+            ];
+
+            foreach ($a_expressions as $e) {
+                preg_match("/{$e}/", $line, $matches);
+
+                if ($matches) {
+                    $id = trim(trim($matches[1]) . " " . trim($matches[2]));
+                    @$a_total[$id]++;
+                } 
+            }
+        }
+
+        arsort($a_total);
+
+        $a_total = array_slice($a_total, 0, 30);
+        
+        return $a_total;
+    }
+}
